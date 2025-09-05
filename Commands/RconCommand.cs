066477fa3556326bc46b124235073a63ab2cfbf8
Tristan <menaras159@gmail.com>
2025-09-05 18:23:22 +0400

    removed useless code

--- Commands/RconCommand.cs
+++ Commands/RconCommand.cs
@@ -1,18 +1,9 @@
-﻿using BepInEx;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace ValheimRcon.Commands
 {
     public abstract class RconCommand : IRconCommand
     {
-        private static readonly string ResultFilePath = Path.Combine(Paths.CachePath, "result.txt");
-
-        public RconCommand()
-        {
-            FileHelpers.EnsureDirectoryExists(ResultFilePath);
-        }
-
         public abstract string Command { get; }
 
         public abstract string Description { get; }
