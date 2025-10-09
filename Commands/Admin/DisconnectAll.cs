31dcd57478db99e54c68e1cb3f49980e96e1f903
Tristan <menaras159@gmail.com>
2025-10-09 15:23:36 +0400

    added disconnect all command

---
+++ Commands/Admin/DisconnectAll.cs
@@ -0,0 +1,15 @@
+﻿namespace ValheimRcon.Commands.Admin
+{
+    internal class DisconnectAll : RconCommand
+    {
+        public override string Command => "disconnectAll";
+
+        public override string Description => "Disconnects all players from the server.";
+
+        protected override string OnHandle(CommandArgs args)
+        {
+            ZNet.instance.SendDisconnect();
+            return "All players have been disconnected from the server.";
+        }
+    }
+}
